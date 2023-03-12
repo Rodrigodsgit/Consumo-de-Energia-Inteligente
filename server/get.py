143f17d1b7f473dbf136522b5f1ca6bc1f007b39
Rodrigo Damasceno <damasceno.s.rodrigo@gmail.com>
2023-03-12 18:17:20 -0300

    Add: Fatura

--- server/get.py
+++ server/get.py
@@ -4,8 +4,20 @@ def get(endpoint, backup):
 
     endpoint = endpoint.split("/")
     if endpoint[1] == "historic":
-        for key, value in backup.items():
-            if key == endpoint[2]:
-                res = alert({},value)
-                res.update({key:value})
-                return res
+        historic = backup.get(endpoint[2]," ")
+        if historic != " ":
+            res = alert({},historic)
+            res.update({endpoint[2]:historic})
+            return res
+        else:
+            return None
+    elif endpoint[1] == "invoice":
+        historic = backup.get(endpoint[2]," ")
+        if historic != " ":
+            res = alert({},historic)
+            spent = historic[-1][0]
+            value = spent * 0.8
+            res.update({"invoice":value})
+            return res
+        else:
+            return None
